db30a73ba878a85c076b33194f34b948b194d82d
sumaiyarimu22 <sumaiyarimu22@gmail.com>
2023-11-08 16:30:42 +0600

    slider added

--- app/(home)/_components/Slider.tsx
+++ app/(home)/_components/Slider.tsx
@@ -1,7 +1,55 @@
 'use client';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation, Autoplay, Pagination } from 'swiper/modules';
+import 'swiper/css/pagination';
+import { data } from '@/data/SliderContents';
+import Image from 'next/image';
+import Link from 'next/link';
+import { cn } from '@/app/libs/utils';
 
 const Slider = () => {
-  return <section className='h-[calc(100vh-5rem)]'>Slider</section>;
+  return (
+    <section className='h-[calc(100vh-5rem)]'>
+      <Swiper
+        navigation={true}
+        pagination={{ clickable: true }}
+        modules={[Navigation, Pagination, Autoplay]}
+        loop={true}
+        grabCursor={true}
+        speed={500}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+        className='mySwiper h-full w-full'
+      >
+        {data.map((slide) => (
+          <SwiperSlide key={slide.heading} className='relative h-full w-full'>
+            <Image
+              src={slide.image}
+              alt={slide.heading}
+              width={1920}
+              height={1080}
+              className='h-full w-full object-cover'
+            />
+            {/* <Overlay /> */}
+            <div className='sp container absolute bottom-0 left-0 right-0 top-0 flex h-full w-full flex-col items-start justify-center gap-5 text-white'>
+              <h1>{slide.heading}</h1>
+              <p className='max-w-6xl'>{slide.subHeading}</p>
+              {/* <Link
+                href='/beauty-packages'
+                className={cn(buttonVariants({ variant: 'secondary' }))}
+              >
+                Browse Beauty Packages
+              </Link> */}
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
+  );
 };
 
 export default Slider;
---
+++ data/SliderContents.ts
@@ -0,0 +1,31 @@
+export const data = [
+  {
+    image: '/assets/images/slide-image-4.jpg',
+    heading: 'Revitalize Your Mind, Body, and Spirit with Healing Touch',
+    subHeading:
+      'Our holistic spa treatments go beyond the surface to restore balance and harmony to your entire being. Allow our experienced therapists to guide you on a journey of self-discovery and renewal. From holistic massages that melt away tension to mindfulness meditation and energy healing, we have a selection of therapies to suit your needs. Feel refreshed, rejuvenated, and ready to face the world with a new sense of vitality and purpose.',
+  },
+  {
+    image: '/assets/images/slide-image-1.jpg',
+    heading: 'Experience Ultimate Relaxation and Rejuvenation',
+    subHeading:
+      'Unwind in our tranquil oasis, surrounded by lush gardens and soothing sounds of nature. Discover a world of serenity and beauty as our expert therapists pamper you with a wide range of treatments designed to rejuvenate your body, mind, and soul. Let the stresses of daily life melt away as you embark on a journey to rediscover your inner peace and natural beauty in an atmosphere of pure tranquility.',
+  },
+  {
+    image: '/assets/images/slide-image-2.jpg',
+    heading: 'Escape to Paradise - Your Journey to Total Bliss',
+    subHeading:
+      'Immerse yourself in our spa sanctuary, a hidden gem nestled in a breathtaking tropical paradise. Our spa offers a holistic approach to well-being, where ancient healing traditions meet modern luxury. From invigorating massages to revitalizing facials, our treatments are designed to transport you to a place of deep relaxation, inner peace, and ultimate rejuvenation. Experience a blissful transformation as you reconnect with your inner self.',
+  },
+  {
+    image: '/assets/images/slide-image-3.jpg',
+    heading: 'Rediscover Your Natural Radiance and Elegance',
+    subHeading: `Let our team of skilled beauty experts enhance your beauty and confidence in a welcoming and luxurious environment. Our comprehensive range of services includes everything from precision haircuts and coloring to rejuvenating facials and soothing body treatments. Step into our world of beauty and elegance, and leave feeling like your most glamorous self. We're here to make you look and feel amazing.`,
+  },
+
+  {
+    image: '/assets/images/slide-image-5.jpg',
+    heading: 'Elevate Your Beauty and Confidence with Our Expert Care',
+    subHeading: `Step into our world of elegance, where we redefine beauty and pamper you like royalty. Our dedicated team of beauty professionals is committed to enhancing your natural beauty and boosting your confidence. Experience a personalized approach to beauty, from makeup and hair styling to nail care and skincare. When you leave our beauty parlor, you'll feel empowered and radiate self-assuredness wherever you go.`,
+  },
+];
